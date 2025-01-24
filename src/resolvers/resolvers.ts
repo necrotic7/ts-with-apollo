a10d02ed259c58becb4756fb240b93bee06abf6d
ZivWu <zivwu@star-link.tech>
2025-01-24 13:41:20 +0800

    [Feat] 改使用parent寫法

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -2,26 +2,13 @@ import {Authors, Books} from "../data/temp"
 const resolvers = {
     Query:{
         books: () => {
-            const response:{
-                id: Number,
-                name: String,
-                author?: {
-                    id: number,
-                    name: string,
-                },
-                publish: string,
-            }[] = [];
-            Books.map(b => {
-                const author = Authors.find(a => a.id == b.author);
-                response.push({
-                    id: b.id,
-                    name: b.name,
-                    author: author,
-                    publish: b.publish,
-                });
-            });
+            return Books;
+        }
+    },
 
-            return response;
+    Book:{
+        author: (parent:{ author: number }) => {
+            return Authors.find(author => author.id == parent.author)
         }
     }
 };
