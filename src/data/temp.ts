842b0591620c6427daac1683e91b7af531175205
ZivWu <zivwu@star-link.tech>
2025-01-24 13:08:50 +0800

    [Feat] add temp data

---
+++ src/data/temp.ts
@@ -0,0 +1,54 @@
+const authors = [
+    {
+      id: 1,
+      name: "Wei",
+    },
+    {
+      id: 2,
+      name: "Yun",
+    },
+  ];
+  
+  const books = [
+    {
+      id: 1,
+      name: "WeiWei Adventure Ep1",
+      author: 1,
+      publish: "Wei's Publish",
+    },
+  
+    {
+      id: 2,
+      name: "WeiWei Adventure Ep2",
+      author: 1,
+      publish: "Wei's Publish",
+    },
+  
+    {
+      id: 3,
+      name: "WeiWei Adventure Ep3",
+      author: 1,
+      publish: "Wei's Publish",
+    },
+    {
+      id: 4,
+      name: "YunYun Adventure Ep1",
+      author: 2,
+      publish: "Yun's Publish",
+    },
+    {
+      id: 5,
+      name: "YunYun Adventure Ep2",
+      author: 2,
+      publish: "Yun's Publish",
+    },
+    {
+      id: 6,
+      name: "YunYun Adventure Ep3",
+      author: 2,
+      publish: "Yun's Publish",
+    },
+  ];
+  
+export const Authors = authors;
+export const Books = books;
